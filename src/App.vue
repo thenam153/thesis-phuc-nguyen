cc1fab0d7712efb0a1cc5f4f53adff11726585f7
Nam The <namnguyen1@beeketing.net>
2021-05-24 15:56:58 +0700

    update feature client

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Screen msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Screen from './components/Screen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Screen
+  }
+}
+</script>
+
+<style>
+#app {  
+}
+</style>
